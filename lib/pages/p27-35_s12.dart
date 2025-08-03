1e911f59332e93d50ac50de00105b5ef0a8f35d3
Saira Alvi <sairaalvi03@gmail.com>
2025-08-03 13:22:43 +0500

    add p35 audio

---
+++ lib/pages/p27-35_s12.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+import '../AudioButton.dart';
+import '../boolGlobleState.dart';
+
+class P27_S11 extends StatefulWidget {
+  const P27_S11({super.key});
+
+  @override
+  State<P27_S11> createState() => _P27_S11State();
+}
+
+class _P27_S11State extends State<P27_S11> {
+  late List<bool> _buttonStates;
+  late final List<String> _audios;
+
+  final audioPlayer = AudioPlayer();
+
+  @override
+  void initState() {
+    super.initState();
+    _buttonStates = List<bool>.filled(33, false);
+    _audios = List.generate(
+      33,
+      (index) => 'audios/p35/${index + 1}.mp3',
+    );
+  }
+
+  @override
+  void dispose() {
+    audioPlayer.dispose();
+    BoolState.setOneButtonRunning(false);
+    BoolState.setOneStreamRunning(false);
+    BoolState.setPlayPause(false);
+    super.dispose();
+  }
+
+  void _triggerButton(int index) {
+    setState(() {
+      _buttonStates[index] = !_buttonStates[index];
+    });
+  }
+
+  Future<void> _triggerButtonsSequentially() async {
+    setState(() {
+      BoolState.toggleOneStreamRunning();
+    });
+
+    for (int i = 0; i < _buttonStates.length; i++) {
+      if (BoolState.playPause == true) {
+        _triggerButton(i);
+        if (i > 0) _triggerButton(i - 1);
+
+        try {
+          await audioPlayer.play(AssetSource(_audios[i]));
+          Duration? duration = await audioPlayer.getDuration();
+          if (duration != null) await Future.delayed(duration);
+        } catch (e) {
+          print('Error playing ${_audios[i]}: $e');
+        }
+
+        if (i == _buttonStates.length - 1) {
+          _triggerButton(_buttonStates.length - 1);
+          setState(() {
+            BoolState.togglePlayPause();
+          });
+        }
+      } else {
+        if (i > 0) _triggerButton(i - 1);
+        break;
+      }
+    }
+
+    setState(() {
+      BoolState.toggleOneStreamRunning();
+    });
+  }
+
+  double adjustedLeft(int col) {
+    double base = 0.035; // left padding
+    double spacing = 0.155; // space between columns
+    return base + col * spacing;
+  }
+
+  double adjustedTop(int row) {
+    double base = 0.367; // top padding adjusted to fit the grid
+    double spacing = 0.09; // space between rows
+    return base + row * spacing;
+  }
+  @override
+  Widget build(BuildContext context) {
+    double buttonWidth = 0.148;
+    double buttonHeight = 0.083;
+
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              'assets/images/img (36).jpg', 
+              fit: BoxFit.fill,
+            ),
+          ),
+
+          for (int i = 0; i < 12; i++)
+            AudioButtonWidget(
+              buttonState: _buttonStates[i],
+              audio: _audios[i],
+              top: adjustedTop(i ~/ 6)+ 0.001,
+              left: adjustedLeft(i % 6),
+              width: buttonWidth,
+              heigt: buttonHeight,
+            ),
+
+             for (int i = 12; i < 15; i++)
+            AudioButtonWidget(
+              buttonState: _buttonStates[i],
+              audio: _audios[i],
+              top: adjustedTop(i ~/ 6),
+              left: adjustedLeft(i % 6)+0.47,
+              width: buttonWidth,
+              heigt: buttonHeight,
+            ),
+          for (int i = 18; i < 36; i++)
+            AudioButtonWidget(
+              buttonState: _buttonStates[i-3],
+              audio: _audios[i-3],
+              top: adjustedTop(i ~/ 6)+0.0465,
+              left: adjustedLeft(i % 6),
+              width: buttonWidth,
+              heigt: buttonHeight,
+            ),
+           
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          if (!BoolState.oneButtonRunning && !BoolState.oneStreamRunning) {
+            setState(() {
+              BoolState.togglePlayPause();
+            });
+            _triggerButtonsSequentially();
+          } else {
+            setState(() {
+              BoolState.togglePlayPause();
+            });
+          }
+        },
+        child: Icon(
+          BoolState.playPause ? Icons.pause : Icons.play_arrow,
+        ),
+      ),
+    );
+  }
+}
