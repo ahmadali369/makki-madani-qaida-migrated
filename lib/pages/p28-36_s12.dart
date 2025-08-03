d9cb57a82f551a3b69b8a408d66043dee9a1e816
Saira Alvi <sairaalvi03@gmail.com>
2025-08-03 13:23:18 +0500

    added p36 audio

--- lib/pages/p28-36_s12.dart
+++ lib/pages/p28-36_s12.dart
@@ -20,10 +20,10 @@ class _P28_S11State extends State<P28_S11> {
   @override
   void initState() {
     super.initState();
-    _buttonStates = List<bool>.filled(45, false);
+    _buttonStates = List<bool>.filled(39, false);
     _audios = List.generate(
-      45,
-      (index) => 'audios/p11/audio_${index + 1}.mp3',
+      39,
+      (index) => 'audios/p36/${index + 1}.mp3',
     );
   }
 
